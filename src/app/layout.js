96f01dc52d83a81618d2dec380dcbf6065285d56
mm-design <mau.micozzi@gmail.com>
2024-10-27 23:53:06 +0000

    Added custom font and deleted old font files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,5 @@
-import localFont from "next/font/local";
+import {space_grotesk } from './utils/fonts'
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -21,9 +10,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` ${space_grotesk} antialiased`}
       >
-        {children}
+        <main className="space_grotesk">{children}</main>
+        
       </body>
     </html>
   );
