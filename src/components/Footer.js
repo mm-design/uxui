1d3e3ad5c516339820c9db5ba9fb49446c1ebd8c
mm-design <mau.micozzi@gmail.com>
2024-11-02 16:48:14 +0000

    Fix footer link to privacy and cookie policy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
                     <li className="mx-2"><a target="blank" href="https://www.dropbox.com/scl/fi/uijuoczyspep80gsprteq/resume-mauro-micozzi.pdf?rlkey=yzyn0rhakbnjre3n6rotmgrwo&e=1&st=l65wuz7o&dl=0" className="inline-block font-normal text-[13px] md:text-[15px] text-[--dark-primary] underline">Resume</a></li>
                     <li className="mx-2"><a href="mailto:mau.micozzi@gmail.com" className="inline-block font-normal text-[13px] md:text-[15px] text-[--dark-primary] underline">mau.micozzi@gmail.com</a></li>
                 </ul>
-                <div className="block text-center sm:flex justify-between items-center pt-28 pb-10">
+                <div className="relative z-[50] block text-center sm:flex justify-between items-center pt-28 pb-10">
                     <ul className="flex justify-center mb-2 sm:mb-0">
                         <li className="mr-4"><Link href="/privacy-policy" className="inline-block font-normal text-[13px] text-[--dark-primary] underline">Privacy Policy</Link></li>
                         <li><Link href="/cookie-policy" className="inline-block font-normal text-[13px] text-[--dark-primary] underline">Cookie Policy</Link></li>
@@ -23,7 +23,7 @@ function Footer() {
                     <p className="inline-block font-normal text-[13px] text-[--dark-secondary]">© Copyright 2024 - All rights reserved</p>               
                 </div>
             </div>
-            <div className="absolute bottom-[-90px] left-[50%] ml-[-285px]"><MauLogoFooter /></div>     
+            <div className="absolute z-[0] bottom-[-90px] left-[50%] ml-[-285px]"><MauLogoFooter /></div>     
         </footer>
     );
 }
